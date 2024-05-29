e761556a57375931469885be32fa9bcfb3e031a9
hrk-mrks <haruki.mrks@gmail.com>
2024-05-29 19:54:24 +0900

    data update

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ col1, col2 = st.columns([4,1])
 with col1:
     '### セッション検索アプリ❄️'
 with col2:
-    st.caption('データ更新日：2024年5月16日')
+    st.caption('データ更新日：2024年5月29日')
 st.caption('''
 セッションの日本語検索アプリ。  
 時点データなので、実際のセッション時間等はリンク先を確認してください。説明文はダブルクリックで全文表示できます。
 ''')
 
-df = pd.read_csv(r'./data/20240516_data_cloud_summit.csv')
+df = pd.read_csv(r'./data/20240529_data_cloud_summit_deepl.csv')
 en_toggle = st.sidebar.toggle('English')
 
 if en_toggle:
